f511cd547815230ccb68fd3fb59c980290e86224
TriYogaF <triyogaf11@gmail.com>
2022-10-13 17:40:12 +0700

    create component CardCabang, DashboardComponent, IconMenu

---
+++ components/CardCabang.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="shadow-md p-4">
+    <div class="header flex justify-between items-center">
+      <p class="font-semibold text-xl md:text-2xl">
+        {{ item.name }}
+      </p>
+      <ButtonComponent :text-fill="'Buka'" />
+    </div>
+    <div class="content flex justify-around text-center pt-4">
+      <div class="profit">
+        <p>Total Pendapatan</p>
+        <p class="text-3xl py-2">
+          {{ item.pendapatan }}
+        </p>
+      </div>
+      <div class="order">
+        <p>Total Order</p>
+        <p class="text-3xl py-2">
+          {{ item.order }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ components/DashboardComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="nav flex justify-between py-10 items-center px-4 md:px-8 lg:px-12"
+  >
+    <div class="title-dashboard">
+      <p class="text-xl lg:text-3xl font-bold">
+        {{ title }}
+      </p>
+    </div>
+    <div
+      class="icon grid justify-items-center grid-cols-3 md:grid-cols-4 lg:grid-cols-5"
+    >
+      <Profile
+        class="icon-profile col-span-2 md:col-span-3 lg:col-span-4 border shadow-md rounded-lg p-2 grid justify-items-center grid-cols-2 md:grid-cols-4 lg:grid-cols-5 items-center mr-1"
+      />
+      <Notification
+        v-if="notif"
+        class="icon-notif border shadow-md rounded-lg p-2 flex justify-center items-center"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    notif: Boolean
+  }
+}
+</script>
+
+<style></style>
