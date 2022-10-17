682d108e6ebb278846da61ad9a8ec7b15c64fbf5
TriYogaF <triyogaf11@gmail.com>
2022-10-17 18:26:17 +0700

    create modal openclose Toko & inputKasir

---
+++ components/InputKasirModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="modal-overlay fixed top-0 bottom-0 right-0 left-0 flex justify-center bg-gray-400/40"
+  >
+    <div class="modal bg-white h-[350px] w-[300px] p-10 my-auto rounded-md">
+      <div class="modal-text pb-2">
+        <p class="font-bold text-center">
+          Modal Kasir
+        </p>
+        <div class="nominal py-4">
+          <p class="pb-1">
+            Nominal
+          </p>
+          <input
+            class="border-2 border-black rounded-md w-[100%]"
+            type="text"
+          >
+        </div>
+        <div class="catatan py-4">
+          <p class="pb-1">
+            Catatan
+          </p>
+          <textarea class="border-2 border-black rounded-md w-[100%]" />
+        </div>
+      </div>
+      <div class="modal-button text-center">
+        <nuxt-link :to="`pos/${id}`">
+          <ButtonComponent text-fill="Selesai" />
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //   emits: ['close-modal']
+  props: {
+    id: String
+  }
+}
+</script>
+
+<style></style>
---
+++ components/OpenCloseModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="modal-overlay fixed top-0 bottom-0 right-0 left-0 flex justify-center bg-gray-400/40"
+  >
+    <div
+      class="modal text-center bg-white h-[150px] w-[500px] p-10 my-auto rounded-md"
+    >
+      <div class="modal-text pb-2">
+        <p class="font-semibold">
+          {{ text.content }}
+        </p>
+      </div>
+      <div class="modal-button">
+        <button @click="$emit('decline')">
+          {{ text.btn1 }}
+        </button>
+        <button @click="$emit('accept')">
+          {{ text.btn2 }}
+        </button>
+        <!-- <ButtonComponent text-fill="Batal" />
+        <ButtonComponent text-fill="Buka" /> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //   emits: ['close-modal']
+  props: {
+    text: Object
+  }
+}
+</script>
+
+<style></style>
