034f3b46565f1df0fad1a82ab9996f37c6571d10
Dery <69612416+derynm@users.noreply.github.com>
2022-10-18 20:13:34 +0700

    add inventory home

---
+++ pages/dashboard/inventory/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col px-7">
+    <button class="shadow-md bg-white py-14 my-2 w-full hover:bg-primary hover:text-white rounded-sm text-2xl md:text-4xl font- bold">
+      Pengiriman Barang Ke Cabang
+    </button>
+    <button class="shadow-md bg-white py-14 my-2 w-full hover:bg-primary hover:text-white rounded-sm text-2xl md:text-4xl font- bold">
+      Pengiriman Barang Ke Costumer
+    </button>
+    <button class="shadow-md bg-white py-14 my-2 w-full hover:bg-primary hover:text-white rounded-sm text-2xl md:text-4xl font- bold">
+      Penerimaan Barang dari Supplier
+    </button>
+    <button class="shadow-md bg-white py-14 my-2 w-full hover:bg-primary hover:text-white rounded-sm text-2xl md:text-4xl font- bold">
+      Lihat status pengiriman barang
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
