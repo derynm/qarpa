ace482b0ccddda6d8acdc5401d3eda0d815fb2f0
Dery <69612416+derynm@users.noreply.github.com>
2022-10-18 14:53:26 +0700

    add modal attendance

---
+++ components/Modal/AttendanceModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="modal-overlay">
+    <div class="modal-body">
+      <img
+        class="object-contain max-h-60"
+        src="@/assets/img/attendance.png"
+        alt="attendance"
+      >
+      <h1 class="text-4xl font-semibold my-10">
+        {{ textModal }}
+      </h1>
+      <ButtonComponent :text-fill="textButton" @click="$emit('closeModal')"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonComponent from '../ButtonComponent.vue'
+export default {
+  components: { ButtonComponent },
+  emits: ['closeModal'],
+  props: {
+    textModal: { type: String, default: null },
+    textButton: { type: String, default: null }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.modal-overlay {
+  background-color: #0000004e;
+  @apply fixed flex justify-center top-0 bottom-0 left-0 right-0 items-center;
+}
+.modal-body {
+  @apply flex flex-col justify-around text-center bg-white shadow rounded-md max-w-lg w-full p-9 mx-2;
+}
+</style>
--- pages/dashboard/attendance/index.vue
+++ pages/dashboard/attendance/index.vue
@@ -1,47 +1,61 @@
 <template>
-  <div class="container mx-auto flex h-screen items-center">
-    <div class="mx-auto max-w-lg">
-      <div class="mx-2 rounded-xl border-2 px-8 py-6 shadow md:mx-0 lg:mx-0">
-        <div
-          class="bg-slate-200 flex justify-center items-center rounded-full min-h-[260px] min-w-[260px] md:min-h-[320px] md:min-w-[320px]"
-        >
-          <h1 class="text-4xl md:text-5xl font-semibold">
-            00 : 00 : 00
-          </h1>
-        </div>
-        <div class="text-center my-6">
-          <h2 class="text-2xl font-semibold">
-            Ahmad Zakkan Ativana
-          </h2>
-          <p>Location :</p>
-        </div>
-        <div class="flex justify-between mx-4 mb-10 font-semibold">
-          <div class="text-center">
-            <p class="text-slate-300">
-              Check In
-            </p>
-            <p class="text-green-500">
-              --:--
-            </p>
+  <div>
+    <div class="container mx-auto flex h-screen items-center">
+      <div class="mx-auto max-w-lg">
+        <div class="mx-2 rounded-xl border-2 px-8 py-6 shadow md:mx-0 lg:mx-0">
+          <div
+            class="bg-slate-200 flex justify-center items-center rounded-full min-h-[260px] min-w-[260px] md:min-h-[320px] md:min-w-[320px]"
+          >
+            <h1 class="text-4xl md:text-5xl font-semibold">
+              00 : 00 : 00
+            </h1>
           </div>
-          <div class="text-center">
-            <p class="text-slate-300">
-              Check In
-            </p>
-            <p class="text-red-600">
-              --:--
-            </p>
+          <div class="text-center my-6">
+            <h2 class="text-2xl font-semibold">
+              Ahmad Zakkan Ativana
+            </h2>
+            <p>Location :</p>
           </div>
+          <div class="flex justify-between mx-4 mb-10 font-semibold">
+            <div class="text-center">
+              <p class="text-slate-300">
+                Check In
+              </p>
+              <p class="text-green-500">
+                --:--
+              </p>
+            </div>
+            <div class="text-center">
+              <p class="text-slate-300">
+                Check In
+              </p>
+              <p class="text-red-600">
+                --:--
+              </p>
+            </div>
+          </div>
+          <button-component :text-fill="'Check In'" class="w-full" />
         </div>
-        <button-component :text-fill="'Check In'" class="w-full" />
       </div>
     </div>
+    <attendance-modal v-if="checkInModalShow" text-button="Lanjutkan" text-modal="Check In Berhasil" />
+    <attendance-modal v-if="checkOutModalShow" text-button="Lanjutkan" text-modal="Check Out Berhasil" />
   </div>
 </template>
 
 <script>
 import ButtonComponent from '~/components/ButtonComponent.vue'
-export default { components: { ButtonComponent } }
+import AttendanceModal from '~/components/Modal/AttendanceModal.vue'
+
+export default {
+  components: { ButtonComponent, AttendanceModal },
+  data () {
+    return {
+      checkInModalShow: false,
+      checkOutModalShow: false
+    }
+  }
+}
 </script>
 
 <style></style>
