ff7028a22546ee3b1ec8b907a4168368114bf276
Dery <69612416+derynm@users.noreply.github.com>
2022-10-19 10:33:45 +0700

    add product card invetory shipping

---
+++ components/wom/CardProductWom.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="grid grid-cols-12 shadow-md p-4">
+    <div class="max-h-[72px] max-w-[72px] col-span-2 mx-auto">
+      <img class="rounded-full" src="@/assets/image.png" alt="product">
+    </div>
+    <div class="col-span-7">
+      <h3 class="font-semibold">{{ product_name }}</h3>
+      <p>{{ stock }}</p>
+      <p>Rp.{{ price }}</p>
+    </div>
+    <div class=" flex justify-around items-center col-span-5 md:col-span-3 pt-7 md:pt-0">
+      <button
+        class="bg-primary w-[36px] h-[36px] rounded text-white font-extrabold"
+      >
+        -
+      </button>
+      <p>2</p>
+      <button
+        class="bg-primary w-[36px] h-[36px] rounded text-white font-extrabold"
+      >
+        +
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product_name: { type: String, default: null },
+    stock: { type: String, default: null },
+    price: { type: String, default: null }
+  }
+}
+</script>
+
+<style></style>
---
+++ pages/dashboard/inventory-shipping/shipping-branch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="relative h-screen mx-2 md:mx-24">
+    <div class="bg-white p-3 shadow">
+      <div class="grid grid-cols-1 gap-5 md:grid-cols-2">
+        <card-product-wom
+          v-for="(product, index) in dummyProduct"
+          :key="index"
+          :product_name="product.product_name"
+          :price="product.price"
+          :stock="product.stock"
+        />
+      </div>
+      <button-component
+        class="absolute right-0 bottom-5"
+        :text-fill="'Kirim Barang'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import CardProductWom from '~/components/wom/CardProductWom.vue'
+import ButtonComponent from '~/components/ButtonComponent.vue'
+export default {
+  components: { CardProductWom, ButtonComponent },
+  data () {
+    return {
+      dummyProduct: [
+        {
+          product_name: 'cappuccino',
+          stock: '120',
+          price: '15000'
+        },
+        {
+          product_name: 'Bacon & Egg Toast',
+          stock: '190',
+          price: '20000'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
