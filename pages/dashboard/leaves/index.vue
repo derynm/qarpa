96843b1fcc904b725238354f1755f8d2ae3aa4b8
TriYogaF <triyogaf11@gmail.com>
2022-10-20 22:16:40 +0700

    create leaves-management & daftar-pegawai page

---
+++ pages/dashboard/leaves/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="h-screen mx-4 md:mx-12">
+    <div class="btn flex flex-row-reverse my-4">
+      <nuxt-link to="leaves/daftar-pegawai">
+        <ButtonComponent text-fill="Daftar Pegawai" />
+      </nuxt-link>
+    </div>
+    <div class="p-3">
+      <div class="flex">
+        <ButtonComponent
+          text-fill="Request"
+          class="w-full"
+          @clicked="isRequest = true"
+        />
+        <div class="border-l-2 mx-5" />
+        <ButtonComponent
+          text-fill="Approve"
+          class="w-full"
+          @clicked="isRequest = false"
+        />
+      </div>
+      <div class="my-5">
+        <div v-show="isRequest">
+          <LeavesCardKaryawan class="border p-2" />
+          <LeavesCardKaryawan class="border p-2" />
+        </div>
+        <div v-show="!isRequest">
+          <p>Approved component</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+export default {
+  layout: 'dashboard-pos',
+  data () {
+    return {
+      isRequest: true
+    }
+  },
+  created () {
+    this.setPageTitle('Leaves Management')
+  },
+  methods: {
+    ...mapMutations(['setPageTitle'])
+  }
+}
+</script>
+
+<style></style>
