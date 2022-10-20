79cb3d72177b2bff988f3e746b7cb7b8f29a71cc
TriYogaF <triyogaf11@gmail.com>
2022-10-20 17:23:19 +0700

    create dashboard stok, cardBarang component, tambah-produk & edit-produk page

---
+++ layouts/dashboard-stock.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <DashboardComponent title="Inventory Stock" />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/dashboard/stock/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container mx-auto px-4 md:px-8 lg:px-12">
+    <div class="container-inner shadow-md p-4">
+      <div class="total-stok text-center shadow-md py-5 mb-4">
+        <p class="font-semibold text-xl">
+          Total Stok Barang Inventori
+        </p>
+        <p class="font-semibold text-2xl">
+          570
+        </p>
+      </div>
+      <div class="kelola-stok">
+        <div class="title">
+          <p class="font-semibold text-xl">
+            Kelola Stok
+          </p>
+        </div>
+        <div class="content">
+          <div class="tab-btn flex justify-evenly my-5">
+            <p>Stok Jualan</p>
+            <p>Stok Suplai</p>
+          </div>
+          <div class="dynamic-component flex flex-col gap-3">
+            <StokCardBarang />
+            <StokCardBarang />
+            <StokCardBarang />
+          </div>
+          <div class="btn flex justify-center my-4">
+            <ButtonComponent text-fill="+ Tambah Produk" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'dashboard-stock'
+}
+</script>
+
+<style></style>
