ee08b85c7377ada494a6da28d48c78048b8bff7c
Dery <69612416+derynm@users.noreply.github.com>
2022-10-26 00:28:17 +0700

    add input field component (basic,email,password) & fix layout navigation

---
+++ components/inputField/emailInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <fieldset
+    class="my-2 flex rounded-md border-2 border-solid border-black p-1 bg-white"
+  >
+    <legend class="ml-6 px-2 text-lg">
+      {{ label }}
+    </legend>
+    <IconsMessageIcon class="ml-2" />
+    <input
+      type="email"
+      :placeholder="placeholder"
+      :value="value"
+      class="w-full px-3 pb-2"
+      required
+      @input="updateValue($event.target.value)"
+    >
+  </fieldset>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { type: String, default: null },
+    placeholder: { type: String, default: null },
+    value: { type: String, default: '' }
+  },
+  methods: {
+    updateValue (value) {
+      this.$emit('input', value)
+    }
+  },
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+input {
+  background-color: rgba(255, 255, 255, 0);
+}
+</style>
