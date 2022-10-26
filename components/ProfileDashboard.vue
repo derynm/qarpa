adce8be42d454f7e318a6ae8600c84e76e5780aa
TriYogaF <triyogaf11@gmail.com>
2022-10-26 19:09:40 +0700

    add profileDashboard & highlightDashboard component

---
+++ components/ProfileDashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="profile flex justify-between bg-[#344397] text-white items-center rounded-xl mt-[-30px]"
+  >
+    <div class="left flex items-center">
+      <IconsPosIcon class="rounded-full p-2" />
+      <div class="bio">
+        <p class="name">
+          Nadya Loppies
+        </p>
+        <p class="role">
+          Pemilik Usaha
+        </p>
+      </div>
+    </div>
+    <div class="right pr-4">
+      <IconsArrowRight />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
