853220c78c9bdc50ffa366a5291d32c493cd3941
TriYogaF <triyogaf11@gmail.com>
2022-10-31 22:57:41 +0700

    edit component PoS & create CardProductDetail

--- components/pos/CardCabang.vue
+++ components/pos/CardCabang.vue
@@ -1,22 +1,33 @@
 <template>
-  <div class="shadow-md p-4">
+  <div class="border-2 border-[#344397] rounded-md shadow-md p-4">
     <div class="header flex justify-between items-center">
-      <p class="font-semibold text-xl md:text-2xl">
+      <p class="font-semibold text-xl">
         {{ item.name }}
       </p>
-      <ButtonComponent text-fill="Buka" @clicked="validateModal = true" />
+      <ButtonComponent
+        class="p-2"
+        text-fill="Buka"
+        @clicked="validateModal = true"
+      />
+    </div>
+    <div class="date py-2">
+      <p>05 Oktober 2022</p>
     </div>
     <div class="content flex justify-around text-center pt-4">
       <div class="profit">
-        <p>Total Pendapatan</p>
-        <p class="text-3xl py-2">
+        <p>
           {{ item.pendapatan }}
+        </p>
+        <p class="font-semibold">
+          Total Pendapatan
         </p>
       </div>
       <div class="order">
-        <p>Total Order</p>
-        <p class="text-3xl py-2">
+        <p>
           {{ item.order }}
+        </p>
+        <p class="font-semibold">
+          Total Order
         </p>
       </div>
     </div>
@@ -26,7 +37,11 @@
       @decline="validateModal = false"
       @accept="acceptBtn"
     />
-    <ModalInputKasir v-show="inputKasir" :id="item.id" />
+    <ModalInputKasir
+      v-show="inputKasir"
+      :id="item.id"
+      @decline="inputKasir = false"
+    />
   </div>
 </template>
 
@@ -43,9 +58,9 @@ export default {
       inputKasir: false,
       emit: 'click-button',
       modalText: {
-        content: 'Apakah Anda Yakin ingin buka Toko',
-        btn1: 'Batal',
-        btn2: 'Buka'
+        content: 'Yakin ingin Buka Toko Sekarang?',
+        btn1: 'Tidak',
+        btn2: 'Yakin'
       }
     }
   },
