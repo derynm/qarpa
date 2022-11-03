ce3bd30c584db5f4ab344ff51e0ed28e0d57d311
TriYogaF <triyogaf11@gmail.com>
2022-11-03 20:02:24 +0700

    create cuti page for owner & karyawan including component and funtion

---
+++ components/cuti/cardCuti.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-if="filterValue == filter || filterValue == ''" class="border-b pb-2">
+    <div class="card-cuti flex justify-between p-2">
+      <div class="left">
+        <p>{{ jenisIzin }}</p>
+        <p>Tanggal Mulai</p>
+        <p>{{ tglMulai }}</p>
+      </div>
+      <div class="right text-right flex flex-col items-end">
+        <p :class="colorStatus">
+          {{ status }}
+        </p>
+        <p>Tanggal Berakhir</p>
+        <p>{{ tglBerakhir }}</p>
+      </div>
+    </div>
+    <div v-if="showButton" class="btn flex justify-evenly">
+      <ButtonGlobal
+        class="min-w-[140px]"
+        text="Setuju"
+        padding="py-2 px-4"
+        color="bg-primary"
+      />
+      <ButtonGlobal
+        class="min-w-[140px]"
+        text="Tidak Setuju"
+        padding="py-2 px-4"
+        outlined="true"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    jenisIzin: { type: String, default: '' },
+    filterValue: { type: String, default: '' },
+    filter: { type: String, default: '' },
+    status: { type: String, default: '' },
+    tglMulai: { type: String, default: '' },
+    tglBerakhir: { type: String, default: '' },
+    showButton: { type: Boolean, default: false }
+  },
+  data () {
+    return {
+      // showButton: false
+    }
+  },
+  computed: {
+    colorStatus () {
+      return {
+        'bg-[#f7b548] p-1 w-fit text-white': this.status === 'Menunggu',
+        'bg-[#6db570] p-1 w-fit text-white': this.status === 'Disetujui',
+        'bg-[#d61c4e] p-1 w-fit text-white': this.status === 'Ditolak'
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,21 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'project-qapra',
+    title: "project-qapra",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['./assets/css/main.css'],
+  css: ["./assets/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -26,23 +26,23 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxt/postcss8'
+    "@nuxtjs/tailwindcss",
+    "@nuxt/postcss8",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/auth-next'
+    "@nuxtjs/axios",
+    "@nuxtjs/auth-next",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://qarpa-dev-backend.herokuapp.com/api/v1/'
+    baseURL: "https://qarpa-dev-backend.herokuapp.com/api/v1/",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -50,35 +50,34 @@ export default {
     postcss: {
       plugins: {
         tailwindcss: {},
-        autoprefixer: {}
-      }
-    }
+        autoprefixer: {},
+      },
+    },
   },
 
   auth: {
     strategies: {
       local: {
         token: {
-          property: 'token',
-          global: true,
+          property: "token",
           required: true,
-          type: false
+          type: "Bearer",
         },
         user: {
           property: false,
         },
         endpoints: {
-          login: { url: 'users/auth/signin', method: 'post' },
+          login: { url: "users/auth/signin", method: "post" },
           user: false,
-          logout: false
+          logout: false,
         },
-      }
+      },
     },
     redirect: {
-      login: '/user/login',
-      logout: '/user/login',
-      callback: '/login',
-      home: '/dashboard'
-    }
-  }
-}
+      login: "/user/login",
+      logout: "/user/login",
+      callback: "/login",
+      home: "/dashboard",
+    },
+  },
+};
