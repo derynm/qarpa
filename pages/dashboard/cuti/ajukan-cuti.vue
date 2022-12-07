efe3556a812b2fd6edfe27c3abf3b2d7ca131d1e
TriYogaF <triyogaf11@gmail.com>
2022-12-07 13:15:27 +0700

    add validation in ajukan-cuti form

--- pages/dashboard/cuti/ajukan-cuti.vue
+++ pages/dashboard/cuti/ajukan-cuti.vue
@@ -57,6 +57,7 @@
           color="bg-primary"
           class="w-full"
           padding="p-2"
+          :disabled="isDisabled"
         />
       </div>
     </form>
@@ -74,7 +75,8 @@ export default {
         catatan: '',
         tglMulai: null,
         tglBerakhir: null
-      }
+      },
+      isDisabled: true
     }
   },
   created () {
@@ -87,7 +89,33 @@ export default {
       console.log(this.dataCuti)
       this.$store
         .dispatch('cuti/postNewCuti', this.dataCuti)
-        .then(this.$router.push('/dashboard/cuti'))
+        .then(this.$router.replace('/dashboard/cuti'))
+    },
+    disabledButton () {
+      if (
+        this.dataCuti.jenisIzin &&
+        this.dataCuti.catatan &&
+        this.dataCuti.tglMulai &&
+        this.dataCuti.tglBerakhir !== null
+      ) {
+        this.isDisabled = false
+      } else {
+        this.isDisabled = true
+      }
+    }
+  },
+  watch: {
+    'dataCuti.jenisIzin' () {
+      this.disabledButton()
+    },
+    'dataCuti.catatan' () {
+      this.disabledButton()
+    },
+    'dataCuti.tglMulai' () {
+      this.disabledButton()
+    },
+    'dataCuti.tglBerakhir' () {
+      this.disabledButton()
     }
   }
 }
