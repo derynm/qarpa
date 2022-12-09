d47ba35c92954720f9be460d43ea2f45a3e99283
TriYogaF <triyogaf11@gmail.com>
2022-12-09 17:10:55 +0700

    fix branch keuangan employee

--- components/keuangan/cardLaporan.vue
+++ components/keuangan/cardLaporan.vue
@@ -2,7 +2,7 @@
   <div class="card-item flex flex-col p-2 gap-2 border text-center rounded-md">
     <p>{{ text }}</p>
     <p class="font-semibold">
-      {{ money }}
+      Rp.{{ toRupiah(money) }}
     </p>
   </div>
 </template>
@@ -12,6 +12,13 @@ export default {
   props: {
     text: { type: String, default: '' },
     money: { type: Number, default: null }
+  },
+  methods: {
+    toRupiah (ele) {
+      return new Intl.NumberFormat('id-ID', {
+        currency: 'IDR'
+      }).format(ele)
+    }
   }
 }
 </script>
