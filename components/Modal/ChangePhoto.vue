77f55bb2da871e2deb79c66d6748a4158c2efebb
Dery <69612416+derynm@users.noreply.github.com>
2022-12-15 11:47:59 +0700

    add validation size image upload in profile

--- components/Modal/ChangePhoto.vue
+++ components/Modal/ChangePhoto.vue
@@ -1,20 +1,36 @@
 <template>
-  <div class="modal-overlay" @click="$emit('closeModal')">
-    <div class="modal-body" @click.stop="">
+  <div class="modal-overlay">
+    <div class="modal-body">
       <div class="flex flex-col items-center">
         <div v-if="!isLoading" class="flex flex-col items-center">
-          <input type="file" accept="image/*" @change="handleInput($event)">
-          <div class="flex mt-5">
+          <input
+            type="file"
+            class="file:bg-primary file:rounded-md file:text-white file:border-none file:px-2 file:py-1"
+            accept="image/png, image/jpeg, image/jpg"
+            @change="handleInput($event)"
+          >
+          <div v-if="imageValid" class="pl-3 my-1 flex">
+            <IconsWarningIcon />
+            <p class="ml-1 text-danger text-[10px] font-semibold">
+              Ukuran gambar maksimmum 1 MB
+            </p>
+          </div>
+          <p class="ml-1 my-1 text-slate-400 text-[10px] font-semibold">
+            Ukuran gambar yang di upload maksimal 1MB dengan format
+            .jpeg/.jpg/.png
+          </p>
+          <div class="flex mt-2">
             <button-global
               text="Batal"
               :outlined="true"
-              padding="py-2 px-2 min-w-[90px] mx-3"
+              padding="py-1 px-2 min-w-[90px] mx-3"
               @click="$emit('closeModal')"
             />
             <button-global
               text="Update"
               color="bg-primary"
-              padding="py-2 px-2 min-w-[90px] mx-3"
+              padding="py-1 px-2 min-w-[90px] mx-3"
+              :disabled="isDisable"
               @click="handleUpdate"
             />
           </div>
@@ -26,30 +42,50 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
   emits: ['closeModal'],
   data () {
     return {
-      dataPhoto: null
+      dataPhoto: null,
+      imageValid: null,
+      isDisable: true,
+      isLoading: false
+    }
+  },
+  watch: {
+    dataPhoto () {
+      if (this.dataPhoto) {
+        this.isDisable = false
+      } else {
+        this.isDisable = true
+      }
     }
   },
   methods: {
     handleInput (e) {
       this.dataPhoto = e.target.files[0]
+      if (this.dataPhoto?.size > 1100000) {
+        this.imageValid = true
+        setTimeout(() => {
+          this.imageValid = false
+        }, '5000')
+        this.dataPhoto = null
+      }
     },
     handleUpdate () {
+      this.isLoading = true
       this.$store
         .dispatch('updateNewProfile', this.dataPhoto)
         .then(() => {
           this.$emit('closeModal')
+          this.isLoading = false
           this.$router.go()
         })
-        .catch(() => alert('Gagal merubah avatar'))
+        .catch(() => {
+          this.isLoading = false
+          alert('Gagal merubah avatar')
+        })
     }
-  },
-  computed: {
-    ...mapState('isLoading')
   }
 }
 </script>
@@ -60,6 +96,6 @@ export default {
   @apply fixed flex justify-center top-0 bottom-0 left-0 right-0 items-center;
 }
 .modal-body {
-  @apply flex flex-col justify-around text-center bg-white shadow rounded-md max-w-lg w-full h-44 p-3 mx-2;
+  @apply flex flex-col justify-around text-center bg-white shadow rounded-md max-w-lg w-full max-h-48 px-3 py-7 mx-2;
 }
 </style>
